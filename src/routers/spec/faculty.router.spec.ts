437f011455efbb1fd35df590160b43cd3ae343eb
Hadymohammed <Hadymohamed2002@gmail.com>
2023-02-21 01:07:38 +0200

    add : faculty (GET : v0/facutly/) unit test

--- src/routers/spec/faculty.router.spec.ts
+++ src/routers/spec/faculty.router.spec.ts
@@ -1,17 +1,27 @@
-import { response } from 'express';
+import { Response } from 'express';
+import request from 'superagent';
 import supertest from 'supertest';
 import app from '../../app';
+import { Faculty } from '../../models/faculty.model';
+import haveProperities from '../../utilities/objectBodyHaveProps.utility';
 
-const request=supertest(app);
+const supetRequest=supertest(app);
 describe("Faculty routes suit",()=>{
-    describe("GET : v0/test/ ",()=>{
-
-        it("Should response with array of faculty",async ()=>{
-            const res= await request.get('v0/test/');
-
-            console.log(res.body);
+    describe("GET : v0/faculty/ ",()=>{
+        let res:request.Response;
+        beforeAll(async()=>{
+            res= await supetRequest.get('/v0/faculty/');
+        })
+        it("Should response with status(200)",async ()=>{
             expect(res.status).toEqual(200);
         })
-
+        it("Should return an array of objects (faculties) ",async()=>{
+            expect(res.body).toBeInstanceOf(Array);
+        })
+        it("Should faculty object has faculty_id,arabic_name,english_name",async()=>{
+            const faculty=res.body[0];
+            const expectedKeys=['faculty_id','arabic_name','english_name'];
+            expect(haveProperities(faculty,expectedKeys)).toEqual(true);
+        })
     })
 })
