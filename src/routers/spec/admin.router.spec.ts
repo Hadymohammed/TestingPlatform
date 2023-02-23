6a15aca74740b74d0b24b0f64f6f1230652188e4
Hadymohammed <Hadymohamed2002@gmail.com>
2023-02-24 00:05:51 +0200

    add : admin (POST : v0/admin/register) unit test

--- src/routers/spec/admin.router.spec.ts
+++ src/routers/spec/admin.router.spec.ts
@@ -2,6 +2,7 @@ import supertest, { Request, Response } from "supertest";
 import request from 'superagent';
 import app from "../../app";
 import haveProperities from "../../utilities/objectBodyHaveProps.utility";
+import { Admin } from "../../models/admins.model";
 
 const superTest=supertest(app);
 
@@ -22,5 +23,46 @@ describe('Admin route suit',()=>{
             const expectedKeys=["admin_id","type_id","national_id","arabic_name","english_name","username","phone","faculty_id"];
             expect(haveProperities(faculty,expectedKeys)).toEqual(true);
         })
+    })
+    /*
+    !! unique testing should be add
+    (username,national_id)
+    */
+    describe('POST : v0/admin/register',()=>{
+        let res:request.Response;
+        const admin:Admin={
+            arabic_name:'مؤقت',
+            english_name:'Temporary',
+            username:'temp',
+            national_id:'1122',
+            password:'12345678',
+            phone:'0101313',
+            type_id:1,
+            faculty_id:1
+        }
+        beforeAll(async()=>{
+            res=await superTest.post('/v0/admin/register').send(admin);
+        })
+        it("Should response with status(200)",async ()=>{
+            expect(res.status).toEqual(200);
+        })
+        it("Should return an object (admin) ",async()=>{
+            expect(res.body).toBeInstanceOf(Object);
+        })
+        it("Should response body object has admin_id,type_id,national_id,arabic_name,english_name,username,phone,faculty_id",async()=>{
+            const resAdmin=res.body;
+            const expectedKeys=["admin_id","type_id","national_id","arabic_name","english_name","username","phone","faculty_id"];
+            expect(haveProperities(resAdmin,expectedKeys)).toEqual(true);
+        })
+        it("Should response body equals to admin object passed in the request",async()=>{
+            const resAdmin=res.body;
+            delete resAdmin.admin_id;
+            delete resAdmin.password;
+            
+            const tempAdmin=admin;
+            delete tempAdmin.password;
+            expect(resAdmin).toEqual(admin);
+        })
+
     })
 })
