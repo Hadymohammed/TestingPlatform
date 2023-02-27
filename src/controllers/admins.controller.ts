473b9b5fe81598a2f11631ee3250f5121403dcd7
Hadymohammed <Hadymohamed2002@gmail.com>
2023-02-27 03:02:54 +0200

    edit : send token in login/register as cookie | add : getAdminProfile

--- src/controllers/admins.controller.ts
+++ src/controllers/admins.controller.ts
@@ -3,8 +3,15 @@ import { Request, Response } from 'express';
 import { varifyAdmin } from '../services/varifyUser.services';
 import hash from '../services/hash.services';
 import missingKeys from '../services/varifyRequestBody.services';
-import { generateAdminToken } from '../services/tokens.services';
+import { AdminToken, generateAdminToken } from '../services/tokens.services';
+import jwt_decode from 'jwt-decode';
+import adminTypeModel from '../models/admin_type.model';
+import facultyModel from '../models/faculty.model';
+import dotenv from 'dotenv';
+
 const adminEntity = new AdminModel();
+dotenv.config();
+const secret = process.env.JWT_SECRET as string;
 
 const index = async (req: Request, res: Response): Promise<void> => {
     try {
@@ -87,7 +94,7 @@ const login = async (req: Request, res: Response): Promise<void> => {
         if (admin) {
             delete admin.password;
             const token=generateAdminToken(admin);
-            res.header({token}).send(admin);
+            res.cookie('token',token, { maxAge: 900000, httpOnly: true }).send(admin);
         } else res.status(401).send('Wrong national Id or password');
     } catch (err) {
         res.status(500).send("Internal server error");
@@ -127,7 +134,7 @@ const register = async (req: Request, res: Response): Promise<void> => {
         if (dbAdmin) {
             delete dbAdmin.password;
             const token=generateAdminToken(admin);
-            res.header({token}).send(dbAdmin);
+            res.cookie('token',token, { maxAge: 900000, httpOnly: true }).send(dbAdmin);
         } else {
             res.status(422).send('Wrong data');
         }
@@ -135,6 +142,46 @@ const register = async (req: Request, res: Response): Promise<void> => {
         res.status(500).send("Internal server error");
     }
 };
+const getAdminProfile =async(req: Request, res: Response): Promise<void> => {
+    const adminTypeEntity=new adminTypeModel();
+    const facultyEntity=new facultyModel();
+    try{
+        const token=req.cookies.token;
+        const {admin_id}=jwt_decode<AdminToken>(token,{header:false});
+        const admin=await adminEntity.getById(admin_id);
+            if(!admin){
+                res.status(400).send({err:"No admin matches the given information"})
+                return
+            }
+            const admin_type=await adminTypeEntity.getById(admin.type_id);
+            const faculty=await facultyEntity.getById(admin.faculty_id);
+            if(!admin_type){
+                res.status(400).send({err:"No admin_type matches the given information"})
+                return
+            }
+            if(!faculty){
+                res.status(400).send({err:"No faculty matches the given information"})
+                return
+            }
+            admin.type=admin_type.type;
+            admin.faculty=faculty.arabic_name;
+            //const admin_tests=await adminEntity.getTests(admin_id);
+            const pastTests=await adminEntity.getPastTests(admin_id);
+            const commingTests=await adminEntity.getCommingTests(admin_id);
+            res.send({
+                admin_id:admin?.admin_id,
+                arabic_name:admin?.arabic_name,
+                english_name:admin?.english_name,
+                faculty:admin?.faculty,
+                type:admin?.type,
+                pastTests:pastTests,
+                commingTests:commingTests
+            })
+        }catch(err){
+            res.status(500).send("Internal server error ");
+        }
+       
+}
 const getAllTests=async (req:Request,res:Response):Promise<void>=>{
     const missing=missingKeys(req,["admin_id"]);
     if(missing.length){
@@ -154,4 +201,4 @@ const getAllTests=async (req:Request,res:Response):Promise<void>=>{
         res.status(500).send("Internal server error");
     }
 }
-export { index, getById, getByNational, getByUsername, login, register,getAllTests };
+export { index, getById, getByNational, getByUsername, login, register,getAllTests,getAdminProfile };
