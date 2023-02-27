7e56aa8b42b91f800de52cc235bf7583308e4256
Hadymohammed <Hadymohamed2002@gmail.com>
2023-02-27 03:08:34 +0200

    add : adminToken,studentToken interface

--- src/services/tokens.services.ts
+++ src/services/tokens.services.ts
@@ -4,6 +4,15 @@ import { Student } from '../models/students.model';
 import { Admin } from '../models/admins.model';
 
 dotenv.config();
+export interface AdminToken {
+    admin_id: number;
+    type: string;
+    iat:number;
+}
+export interface StudentToken {
+    student_id: number;
+    iat:number;
+}
 const generateStudentToken = (student: Student): string => {
     const secret = process.env.JWT_SECRET as string;
     const student_id=student.student_id;
