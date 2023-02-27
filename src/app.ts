691104b7bb27294622d074b6fc9922b76d143a33
Hadymohammed <Hadymohamed2002@gmail.com>
2023-02-27 03:01:09 +0200

    use new middleware : cookie-parser

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import router from './routers';
 import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
+import cookieParser  from 'cookie-parser';
 
 const app: Application = express();
 const address = '127.0.0.1:3000';
 
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors());
 app.use(json(), helmet(), morgan('dev'));
